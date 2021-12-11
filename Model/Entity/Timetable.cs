57697331fe793cd4be484d6eb996f756f63e7a8c
polina <nalivayko0428@gmail.com>
2021-12-12 00:40:20 +0300

    feat: add main entities

---
+++ Model/Entity/Timetable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Entity
+{
+    public class Timetable
+    {
+        public List<Time> TimeOfFeed { get; set; }
+        public string UserId { get; set; }
+        public string name { get; set; }
+        public string TimetableId { get; set; }
+
+    }
+}
